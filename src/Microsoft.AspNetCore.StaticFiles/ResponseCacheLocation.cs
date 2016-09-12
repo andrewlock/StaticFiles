85e6ccb3165785c96862b1db81cb6abcbf080a70
Sock <sock@andrewlock.net>
2016-09-12 21:04:13 +0100

    Add missing copyright headers

--- src/Microsoft.AspNetCore.StaticFiles/ResponseCacheLocation.cs
+++ src/Microsoft.AspNetCore.StaticFiles/ResponseCacheLocation.cs
@@ -1,4 +1,7 @@
-﻿namespace Microsoft.AspNetCore.StaticFiles
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+namespace Microsoft.AspNetCore.StaticFiles
 {
     /// <summary>
     /// Determines the value for the "Cache-control" header in the response.
